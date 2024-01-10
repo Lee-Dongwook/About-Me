df744c5cb8d2acc9bc004909b7089e6aac845401
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-10 20:41:51 +0900

    remove : Style Component 관련 재구성을 위한 style 폴더 임시 제거

--- profile/src/app/layout.tsx
+++ profile/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from '@/03-styles';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,9 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
