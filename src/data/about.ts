34c6c4e42a823b8b59d9e4e4b137a5bd878c7fce
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-03-21 23:30:04 +0900

    feat : 기본 인적사항 기입

---
+++ src/data/about.ts
@@ -0,0 +1,22 @@
+export const About = [
+  {
+    type: "Name",
+    description: "이동욱",
+  },
+  {
+    type: "Age",
+    description: 26,
+  },
+  {
+    type: "Email",
+    description: "dlehddnr0713@gmail.com",
+  },
+  {
+    type: "Github",
+    description: "https://github.com/Lee-Dongwook",
+  },
+  {
+    type: "Velog",
+    description: "https://velog.io/@dlehddnr99",
+  },
+];
